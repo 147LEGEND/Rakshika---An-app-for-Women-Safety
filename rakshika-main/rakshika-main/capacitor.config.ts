92227b4779c594c7f4412cb1c6cb8c3ff818ecbd
147LEGEND <147goldrulz@gmail.com>
2026-06-01 22:43:07 +0530

    Add files via upload

---
+++ rakshika-main/rakshika-main/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rakshika.app',
+  appName: 'Rakshika',
+  webDir: 'dist'
+};
+
+export default config;
